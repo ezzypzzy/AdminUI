f3fcd6e3f8a8b088cfc445c5c9aeaff39cf32fbe
Asutosh <ashutoshguleria9@gmail.com>
2023-04-18 17:34:24 +0530

    single quotes had to be replaced with &apos; for production build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -157,7 +157,7 @@ export default function Home() {
         <meta name="description" content="Geektrust frontend assignment" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <style>
-          @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap');
+          @import url(&apos;https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap&apos;);
         </style>
       </Head>
 
